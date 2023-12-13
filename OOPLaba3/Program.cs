a525118f9518ab0070d0b2675a569639d70dd7eb
VortexLite <minedinnerbone@gmail.com>
2023-12-13 09:01:27 +0200

    Add project

---
+++ OOPLaba3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace OOPLaba3_1
+{
+    internal class Program
+    {
+        public static int Key = 102;
+        static void Main(string[] args)
+        {
+            // кодіровка
+            Console.OutputEncoding = Encoding.UTF8;
+            string firstName, lastName, poName;
+
+            Console.Write("Введіть прізвище = ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Введіть ім'я = ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Введіть по батькові = ");
+            poName = Console.ReadLine();
+
+            // довжина рядків  і вивід їх даних
+            LenghtString(ref firstName, ref lastName, ref poName);
+
+            // ім'я та кількість літер прізвища
+            NameAndLenght(ref firstName, ref lastName);
+
+        }
+
+        static void LenghtString(ref string firstName, ref string lastName, ref string poName)
+        {
+            Console.WriteLine("Довжина рядків");
+            Console.WriteLine($"{firstName} = {firstName.Length}");
+            Console.WriteLine($"{lastName} = {lastName.Length}");
+            Console.WriteLine($"{poName} = {poName.Length}");
+            
+            Console.WriteLine($"{lastName} {firstName} {poName}");
+            Console.WriteLine("*Студент спеціальності Комп'ютерні науки, другої группи.*\n");
+        }
+
+        static void NameAndLenght(ref string fistName, ref string lastName)
+        {
+            Console.WriteLine($"Ім'я {fistName}\nДовжина прізвища = {lastName.Length}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOPLaba3_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace OOPLaba3_2
+{
+    internal class Program
+    {
+        public static int Key = 102;
+
+        static void Main(string[] args)
+        {
+            // кодіровка
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введіть рядок, щоб його зашифрувати = ");
+            string InputString;
+            InputString = Console.ReadLine();
+
+            Console.WriteLine($"Початкові дані: {InputString}\n");
+
+            Console.WriteLine($"Зашифрований рядок: {Convert.ToBase64String(Encrypt(InputString))}\n");
+
+            Console.WriteLine($"Розшифрований рядок: {Encoding.UTF8.GetString(Decrypt(Encoding.UTF8.GetBytes(InputString)))}\n");
+            Console.ReadLine();
+        }
+
+        public static byte[] Encrypt(string InputString)
+        {
+            byte[] DataEncrypt = Encoding.UTF8.GetBytes(InputString);
+            for (int i = 0; i < DataEncrypt.Length; i++)
+            {
+                DataEncrypt[i] = (byte)(DataEncrypt[i] ^ Key);
+            }
+            return DataEncrypt;
+        }
+
+        public static byte[] Decrypt(byte[] DataEncrypt)
+        {
+            byte[] DataDecrypt = Convert.FromBase64String(Convert.ToBase64String(DataEncrypt));
+            for (int i = 0; i < DataDecrypt.Length; i++)
+            {
+                DataDecrypt[i] = (byte)(DataDecrypt[i] ^ Key);
+            }
+            return DataEncrypt;
+        }
+    }
+}
